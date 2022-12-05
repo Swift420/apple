39ece9d115caf35a30931d7cb42276fee9619236
Swift420 <apollosdavid95@gmail.com>
2022-12-05 11:47:53 +0200

    shopping bag icon

--- components/Header.tsx
+++ components/Header.tsx
@@ -31,11 +31,15 @@ function Header() {
         <Link href='' className='headerLink'>Business</Link>
         
       </div>
-      <div className=''>
+      <div className='flex items-center justify-center gap-x-4 md:w-1/5'>
         <SearchIcon className='headerIcon'/>
-        <div>
-          
+        <Link href='/checkout'>
+        <div className='relative cursor-pointer'>
+          <span className='absolute -right-1 -top-1 z-50 flex h-4 w-4 items-center justify-center rounded-full bg-gradient-to-r from-pink-500 to-violet-500'>5</span>
+          <ShoppingBagIcon className='headerIcon'/>
+       
         </div>
+        </Link>
       </div>
 
     </header>
