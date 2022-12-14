744cb66536095fa819558c230c02f0a21236f2bb
Swift420 <apollosdavid95@gmail.com>
2022-12-14 10:21:54 +0200

    Product typings

--- typings.d.ts
+++ typings.d.ts
@@ -12,6 +12,30 @@ interface Category {
 }
 
 
+interface Image {
+    _key: string;
+    _type: "image";
+    asset: {
+        url: string;
+    }
+}
+
 interface Product {
-    
+     _id: string;
+    _createdAt: string;
+    _updatedAt: string;
+    _rev: string;
+    _type: "product";
+    title: string;
+    price: number;
+    slug: {
+        _type: "slug";
+        current: string;
+    };
+    description: string;
+    category: {
+        _type: "reference";
+        _ref_: string;
+    };
+    image: Image[];
 }
