15569cf2888467c74af00c4e00236ff86cd1bdc6
Swift420 <apollosdavid95@gmail.com>
2022-12-14 16:19:17 +0200

    Checkout Page

--- components/Basket.tsx
+++ components/Basket.tsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import { ShoppingBagIcon } from "@heroicons/react/outline";
+import Link from "next/link";
+import { useSelector } from "react-redux";
+import { selectBasketItems } from "../redux/basketSlice";
 
-const Basket = () => {
+function Basket() {
+  const items = useSelector(selectBasketItems);
+
+  if (items.length === 0) return null;
+
   return (
-    <div>
-      
-    </div>
-  )
+    <Link href="/checkout">
+      <div className="fixed bottom-10 right-10 z-50 flex h-16 w-16 cursor-pointer items-center justify-center rounded-full bg-gray-300">
+        {items.length > 0 && (
+          <span className="absolute -right-2 -top-2 z-50 flex h-7 w-7 items-center justify-center rounded-full bg-gradient-to-r from-pink-500 to-violet-500 text-[10px] text-white">
+            {items.length}
+          </span>
+        )}
+        <ShoppingBagIcon className="headerIcon h-8 w-8" />
+      </div>
+    </Link>
+  );
 }
 
-export default Basket
+export default Basket;
