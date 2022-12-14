009178b3e76c965d92912157a7a5a9d372b51499
Swift420 <apollosdavid95@gmail.com>
2022-12-14 11:24:05 +0200

    Product tabs

--- pages/index.tsx
+++ pages/index.tsx
@@ -66,10 +66,12 @@ const Home = ({categories, products}: Props) => {
 
               ))}
             </Tab.List>
-            <Tab.Panels>
-              <Tab.Panel className='mx-auto max-w-fit pt-10 pb-24 sm:px-4 '>
 
-              </Tab.Panel>
+            <Tab.Panels className='mx-auto max-w-fit pt-10 pb-24 sm:px-4 '>
+              <Tab.Panel className='tabPanel '>{showProducts(0)}</Tab.Panel>
+              <Tab.Panel className='tabPanel '>{showProducts(1)}</Tab.Panel>
+              <Tab.Panel className='tabPanel '>{showProducts(2)}</Tab.Panel>
+              <Tab.Panel className='tabPanel '>{showProducts(3)}</Tab.Panel>
             </Tab.Panels>
          </Tab.Group>
          </div>
